09b1ff49d69628d46fc5ad380e9e92aadf4e4790
Fern Nimneim <pontip_solo@hotmail.com>
2026-09-02 11:44:35 +0700

    Add route for EventSchedule component

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import EventsView from '../views/EventsView.vue'
 import JourneyView from '../views/JourneyView.vue'
 import AchievementsView from '../views/AchievementsView.vue'
+import EventSchedule from '../components/EventSchedule.vue' // 1. import component เข้ามา
 
 const routes = [
   { path: '/', redirect: '/events' },
   { path: '/events', name: 'events', component: EventsView },
+  { path: '/schedule', name: 'schedule', component: EventSchedule }, // 2. เพิ่ม route สำหรับ schedule
   { path: '/journey', name: 'journey', component: JourneyView },
   { path: '/achievements', name: 'achievements', component: AchievementsView },
 ]
