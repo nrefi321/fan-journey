08573cee925070459f5a5fbd07da427df048761d
Fern Nimneim <pontip_solo@hotmail.com>
2026-09-04 10:15:06 +0700

    Refactor TicketStub.vue for image handling and styles

--- src/components/TicketStub.vue
+++ src/components/TicketStub.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="stub" @click="$emit('open', event.id)">
-    <div class="stub-cover" :style="coverStyle">
-      {{ event.emoji }}
+    <!-- มีรูป → แสดงรูป -->
+    <div class="stub-cover" :style="!event.image ? coverStyle : {}">
+      <img
+        v-if="event.image && !imgError"
+        :src="event.image"
+        :alt="event.title"
+        class="stub-img"
+        @error="imgError = true"
+      />
+      <template v-else>
+        {{ event.emoji }}
+      </template>
       <div v-if="attended" class="checked-badge">✓ WAS THERE</div>
     </div>
     <div class="tear-line"></div>
@@ -16,7 +26,7 @@
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { ref, computed } from 'vue'
 import { formatDate } from '../utils/format'
 
 const props = defineProps({
@@ -25,8 +35,24 @@ const props = defineProps({
 })
 defineEmits(['open'])
 
+const imgError = ref(false)
+
 const coverStyle = computed(() => ({
   background: `linear-gradient(135deg, ${props.event.g[0]}, ${props.event.g[1]})`,
 }))
 const formattedDate = computed(() => formatDate(props.event.date))
 </script>
+
+<style scoped>
+.stub-img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center top;
+  position: absolute;
+  inset: 0;
+}
+.stub-cover {
+  position: relative;
+}
+</style>
